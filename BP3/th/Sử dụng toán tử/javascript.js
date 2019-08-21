1c802566c19a6ceea069603cab874a8cd151b6d8
Lthe4th <longkin0159102@gmail.com>
2019-08-21 10:50:20 +0700

    reDo job

---
+++ BP3/th/Sử dụng toán tử/javascript.js
@@ -0,0 +1,8 @@
+var inputWidth = +prompt(" width");
+var inputHeight = +prompt(" height");
+
+var area = inputWidth * inputHeight;
+
+document.write('enter width: ' + '<br>'+ inputWidth + "<br>");
+document.write('enter Height: ' + '<br>'+ inputHeight + "<br>   ")
+document.write("The area is: " + area);
